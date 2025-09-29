7d2c81f4489acf3fc4c91fd1329e9754aebeef98
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-09-29 21:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  const ageInput = document.getElementById("age").value;
+  const nameInput = document.getElementById("name").value;
+
+
+  if (!ageInput || !nameInput) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Create the promise
+  const voterPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput) > 18) {
+        resolve(`Welcome, ${nameInput}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${nameInput}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  voterPromise
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
